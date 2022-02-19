fb96d9b911f2b713f4731d4d54d1c82cebb943c4
RBoraurito <r.boraure@kation.com.do>
2022-02-19 12:00:39 -0400

    ✨ Add set players page

--- src/pages/Players.vue
+++ src/pages/Players.vue
@@ -1,5 +1,86 @@
 <template>
-  <div class="">
-    Players
-  </div>  
+  <section class="flex justify-center items-center h-screen">
+    <div class="p-4 h-fit w-fit">
+      <h1 class="text-3xl text-center font-bold uppercase mb-8">
+        Bowling 2D
+      </h1>
+      <div
+        v-for="(player, index) in players"
+        :key="index"
+        class="mb-4"
+      >
+        <label :for="`player${formattedIndex(index)}`" class="text-lg mb-3 inline-block">
+          Player {{ formattedIndex(index) }}
+        </label>
+        <input
+          v-model="players[index]"
+          type="text"
+          :id="`player${formattedIndex(index)}`"
+          placeholder="Your name"
+          class="block border border-gray-200 rounded-md py-3 px-4 placeholder:text-gray-400 text-gray-700"
+          @input="checkError(index)"
+        >
+        <span v-show="errors[index]" class="text-sm mt-2 text-red-700 inline-block">
+          Please fill with your name
+        </span>
+      </div>
+      <button
+        class="py-3 rounded-md text-lg leading-none bg-green-600 text-white w-full font-bold mb-3 mt-8 disabled:cursor-not-allowed disabled:bg-opacity-50"
+        :disabled="hasError.length"
+        @click="setPlayers"
+      >
+        Start
+      </button>
+      <button
+        class="py-3 rounded-md text-lg leading-none bg-red-600 text-white w-full font-bold"
+        @click="goBack"
+      >
+        Back
+      </button>
+    </div>
+  </section>  
 </template>
+
+<script>
+import { ref } from '@vue/reactivity'
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+import { computed } from '@vue/runtime-core'
+
+export default {
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+
+    const playerCount = 2
+    const players = ref(Array(playerCount).fill(''))
+
+    const errors = ref(Array(playerCount).fill(true))
+    const checkError = (i) => {
+      errors.value[i] = players.value[i] === '' && true
+    }
+    const hasError = computed(() => {
+      return errors.value.filter((e) => e === true)
+    })
+    
+    const setPlayers = () => {
+      if(hasError.value.length) return
+      store.dispatch('setPlayers', players.value)
+      router.push('/game')
+    }
+    const goBack = () => {
+      store.commit('backFromPlayers')
+      router.push('/')
+    }
+    return {
+      goBack,
+      setPlayers,
+      players,
+      formattedIndex: (i) => i += 1,
+      errors,
+      checkError,
+      hasError
+    }
+  },
+}
+</script>
--- src/store.js
+++ src/store.js
@@ -3,12 +3,36 @@ import { createStore } from 'vuex'
 const store = createStore({
   state () {
     return {
-      isStarted: false
+      isStarted: false,
+      players: []
     }
   },
   mutations: {
     begin (state) {
       state.isStarted = true
+    },
+    addPlayer(state, payload) {
+      state.players.push({
+        name: payload,
+        id: crypto.randomUUID(),
+        rounds: Array(10).fill({
+          first: 0,
+          second: 0,
+          type: '',
+          total: 0
+        })
+      })
+    },
+    backFromPlayers(state) {
+      state.isStarted = false
+    }
+  },
+  actions: {
+    setPlayers({state, commit}, payload) {
+      state.players = []
+      payload.forEach(player => {
+        commit('addPlayer', player)
+      })
     }
   }
 })
